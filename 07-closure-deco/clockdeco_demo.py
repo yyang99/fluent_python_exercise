187e2d7c1b1a26125a79832112730ae72a021777
Yufeng Yang <yufeng.yang@bell.ca>
2021-03-06 17:20:26 -0500

    CH7

---
+++ 07-closure-deco/clockdeco_demo.py
@@ -0,0 +1,21 @@
+import time
+from clockdeco import clock
+
+
+@clock
+def snooze(seconds):
+    ''' snooze func'''
+    time.sleep(seconds)
+
+
+@clock
+def factorial(n):
+    ''' factorial func '''
+    return 1 if n < 2 else n * factorial(n - 1)
+
+
+if __name__ == '__main__':
+    print('*' * 40, 'Calling snooze(.123)')
+    snooze(.123)
+    print('*' * 40, 'Calling factorial(6)')
+    print('6! =', factorial(6))
