64fd00bbf06832990614c067e6da93c053c2ae99
Yufeng Yang <yufeng.yang@bell.ca>
2021-05-01 12:13:35 -0400

    CH21

---
+++ ch21_class_metaprog/bulkfood/bulkfood_v8.py
@@ -0,0 +1,24 @@
+import model_v8 as model
+
+class LineItem(model.Entity):
+    description = model.NonBlank()
+    weight = model.Quantity()
+    price = model.Quantity()
+
+    def __init__(self, description, weight, price):
+        self.description = description
+        self.weight = weight
+        self.price = price
+
+    def subtotal(self):
+        return self.weight * self.price
+
+
+if __name__ == '__main__':
+    raisins = LineItem('Gloden raisins', 10, 6.95)
+    print(f"{raisins.subtotal()=}")
+    raisins.weight = 20
+    print(f"{raisins.subtotal()=}")
+
+    for name in LineItem.field_names():
+        print(name)
