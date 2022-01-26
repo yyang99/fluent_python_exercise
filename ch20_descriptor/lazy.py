1152c27a94fcfd288a428f2677a684451fb553fe
yyang99 <yyang99@hotmail.com>
2022-01-25 20:24:22 -0500

    ch20 add lazy.py

--- ch20_descriptor/lazy.py
+++ ch20_descriptor/lazy.py
@@ -5,14 +5,14 @@ class Lazy:
     def __set_name__(self, owner, name):
         self.key = name
 
-    def __get__(self, instance, cls):
+    def __get__(self, instance, owner):
         if instance:
             print(f"calculating {self.func}({instance})")
             value = self.func(instance)
             instance.__dict__[self.key] = value
             return value
         else:
-            return self.func
+            return self
 
 class Rectangle:
     area = Lazy(lambda self: self.width * self.height)
